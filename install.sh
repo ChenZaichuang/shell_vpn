69d3c6ec7028adf52df899486b33d88fc44c1709
zaichuang <zcchen.dev@gmail.com>
2023-04-07 13:57:10 +0800

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ $# -eq 2 ]; then
+  PROXY_NODE_INDEX=$1
+  VPN_PASSWORD=$2
+  echo "Error: Missing argument. Please provide an argument."
+  exit 1
+
+fi
+
+echo "The argument is: $1"
+
 VPN_DIR="./vpn"
 
 # 判断目录是否存在
@@ -7,19 +17,29 @@ if [ ! -d "$VPN_DIR" ]; then
   # 下载zip文件
   curl -O "https://raw.githubusercontent.com/ChenZaichuang/shell_vpn/main/vpn.zip"
   # 提示用户输入密码并解压缩zip文件
-  while true; do
+
+  if [[ ! -v VPN_PASSWORD ]]; then
     echo "Enter VPN password: "
     read -s VPN_PASSWORD </dev/tty
     echo
-    unzip -q -P "$VPN_PASSWORD" vpn.zip -d .
-    if [ $? -eq 0 ]; then
-      # 解压缩成功
-      break
-    else
-      # 解压缩失败，提示用户重新输入密码
-      echo "Incorrect password. Please try again."
-    fi
-  done
+  fi
+
+  unzip -q -P "$VPN_PASSWORD" vpn.zip -d .
+
+  if [ ! $? -eq 0 ]; then
+    # 解压缩失败，提示用户重新输入密码
+    echo "Incorrect password."
+    exit 1
+  fi
+
+  if [[ ! -v PROXY_NODE_INDEX ]]; then
+    echo "Enter proxy node index: "
+    read -s PROXY_NODE_INDEX </dev/tty
+    echo
+  fi
+
+  sed -i "s/{{ PROXY_NODE_INDEX }}/$PROXY_NODE_INDEX/g" vpn/frp/frpc.ini
+
   # 修改文件权限
   chmod +x vpn/start.sh
 fi
