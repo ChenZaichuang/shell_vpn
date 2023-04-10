b072e1a0b56e9eae00d125e031dfcc116de89222
zaichuang <zcchen.dev@gmail.com>
2023-04-11 00:16:35 +0800

    update

--- reinstall.sh
+++ reinstall.sh
@@ -9,4 +9,11 @@ if [ ! $# -eq 5 ]; then
   exit 1
 fi
 
-curl -s "https://raw.githubusercontent.com/ChenZaichuang/shell_vpn/main/install.sh" | bash -s $1 $2 $3 $4 $5
+curl -s "https://raw.githubusercontent.com/ChenZaichuang/shell_vpn/main/install.sh" \
+| VPN_PASSWORD="${VPN_PASSWORD}" \
+| NODE_NAME="${NODE_NAME}" \
+| SINGLE_PORT="${SINGLE_PORT}" \
+| GROUP_PORT="${GROUP_PORT}" \
+| NODE_GROUP="${NODE_GROUP}" \
+| SSH_PORT="${SSH_PORT}" \
+bash
